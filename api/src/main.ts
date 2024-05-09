dfd0a66d4ece5a9de6bc59aedd2b5d8af3641b00
nachocheez <cheskapg@gmail.com>
2024-05-10 07:11:06 +0800

    changed orgin to fdfif format

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,8 +6,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Define CORS options
   const corsOptions: CorsOptions = {
-    origin:[process.env.FE_URL1, process.env.FE_URL2], // Allow requests from this origin
-    credentials: true, // Optional: If you need to send cookies with the request
+    origin: (origin, callback) => {
+      // Check if the request origin is allowed
+      if (
+        [process.env.FE_URL1, process.env.FE_URL2].includes(origin) ||
+        !origin // Allow requests with no origin (e.g., same-origin requests)
+      ) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Deny the request
+      }
+    },    credentials: true, // Optional: If you need to send cookies with the request
   };
   app.enableCors(corsOptions);
 
