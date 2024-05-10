13d0cd87ea6f3238919697324eeacb89cd16238b
nachocheez <cheskapg@gmail.com>
2024-05-10 08:23:33 +0800

    Update main.ts with origin true

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,17 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Define CORS options
   const corsOptions: CorsOptions = {
-    origin: (origin, callback) => {
-      // Check if the request origin is allowed
-      if (
-        [process.env.FE_URL1, process.env.FE_URL2].includes(origin) ||
-        !origin // Allow requests with no origin (e.g., same-origin requests)
-      ) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS')); // Deny the request
-      }
-    },    credentials: true, // Optional: If you need to send cookies with the request
+    origin: true,
+    credentials: true, // Optional: If you need to send cookies with the request
   };
   app.enableCors(corsOptions);
 
